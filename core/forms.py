0422cdbf099d48b2bcc2d711b75ac28791a77bbb
Hassan Diab <hasan.diab.022001@gmail.com>
2023-10-02 17:49:24 +0300

    Crud for all the models plus ui fix

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm,Select,TextInput,DateInput
-from .models import Employee,Role
+from .models import Employee,Role,Leaves
 
 class EmployeeForm(ModelForm):
     
@@ -35,6 +35,9 @@ class EmployeeForm(ModelForm):
             'hourly_rate': TextInput(attrs={
                 'class': "form-control"
                 }),
+            'id': TextInput(attrs={
+                'style': "display:none"
+                }),
         }
 
 class RoleForm(ModelForm):
@@ -44,11 +47,33 @@ class RoleForm(ModelForm):
         model = Role
         fields = ['role_name','off_days']
         widgets = {
+                     
            
             'role_name': TextInput(attrs={
                 'class': "form-control"
                 }),
             'off_days': TextInput(attrs={
+                'class': "form-control"
+                }),
+           
+        }
+
+class LeaveForm(ModelForm):
+    
+    class Meta:
+        
+        model = Leaves
+        fields = ['employee','date','reason']
+        widgets = {
+                     
+           
+                'employee': Select(attrs={
+                'class': "form-control"
+                }),
+                'reason': Select(attrs={
+                'class': "form-control"
+                }),
+                'date': DateInput(attrs={
                 'class': "form-control"
                 }),
            
